42296f8c32b209aec10a8e4f9586f2b04245ce8e
Razvan Pop <esdpop@gmail.com>
2026-03-06 00:18:57 +0100

    -

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,6 +4,7 @@ return {
         vim.lsp.enable("rust_analyzer")
         vim.lsp.enable("clangd")
         vim.lsp.enable("texlab")
+        vim.lsp.enable("qmlls")
 
         vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
         vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
